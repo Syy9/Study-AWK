71296262b941ef63545f11d16d8d439df4c1dd87
Syy <shirakawa.yy@gmail.com>
2018-08-26 22:56:32 +0900

    Add sample

--- 001_basic.sh
+++ 001_basic.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo "Hello, world"
+echo "Hello, world" # Hello, world
+
+echo "foo" | awk '0' # non
+
+echo "foo" | awk '1' # foo
+
+echo "foo" | awk 'A' # foo
+
+echo "foo" | awk '0""' # foo
+
+echo "foo" | awk '$0 == "foo"' # foo
+
+echo "foo" | awk '{print $0 == "foo"}' # syntax error
+
+echo "foo" | awk '{print ($0 == "foo")}' # 1
