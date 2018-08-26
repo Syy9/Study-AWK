3f45152674b35868efc68b1fec47d1f26dde2866
Syy <shirakawa.yy@gmail.com>
2018-08-26 23:34:28 +0900

    Add sample

--- 001_basic.sh
+++ 001_basic.sh
@@ -39,3 +39,63 @@ echo "a b c" | awk '$0 = gsub(/a/, "")' # 1
 echo "a b c" | awk '$0 = gsub(/d/, "")' # non
 
 echo "a b c" | awk '$0 = gsub(/d/, "")""' # 0
+
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■"
+
+seq 1 3
+# 1
+# 2
+# 3
+
+seq 1 3 | awk 'NR == 1 {print $0}' # 1
+seq 1 3 | awk 'NR == 1' # 1
+seq 1 3 | awk 'NR <= 2'
+# 1
+# 2
+
+seq 1 5 | awk 'NR >= 2 && NR <= 4'
+# 2
+# 3
+# 4
+
+seq 1 5 | awk 'NR == 2, NR == 4'
+# 2
+# 3
+# 4
+
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■"
+
+seq 1 10 | awk 'NR % 2 == 0, NR %3 == 0'
+# 2
+# 3
+# 4
+# 5
+# 6
+# 8
+# 9
+# 10
+
+seq 1 3 | awk '/1/' # 1
+
+seq 1 3 | awk '$0 ~ /1/' # 1
+
+seq 1 3 | awk '!/1/'
+# 2
+# 3
+
+seq 1 3 | awk '$0 !~ /1/'
+# 2
+# 3
+
+seq 1 3 | awk '/[12]/'
+# 2
+# 3
+
+ps ax | grep 'awk' | grep -v 'grep' | awk '{print $1}' # some psid
+
+ps ax | awk '/[a]wk/ {print $1}' # some psid
+
